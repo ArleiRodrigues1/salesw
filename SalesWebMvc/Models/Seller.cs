53d8620a9e10d0f03b5e3433599de7dcc22b753a
ArleiRodrigues1 <arlei.freire011@gmail.com>
2023-03-30 12:14:25 -0300

    Ajustando para MySql e primeira migration | novas entidades e segunda migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Insira o nome")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Máximo de 60 e mínimo de 3 caracteres.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Insira o email")]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Insira a data de nascimento")]
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Aniversario { get; set; }
+
+        [Required(ErrorMessage = "Insira o salário")]
+        [Range(100.0, 50000.0, ErrorMessage = "Mínimo de R$100 e máximp de R$5000,00")]
+        [Display(Name = "Salário")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double SalarioBase { get; set; }
+        public Department Department { get; set; }
+        [Required(ErrorMessage = "Selecione o Departamento")]
+        [Display(Name = "Departamento")]
+        public int DepartmentId { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
+        public Seller() { }
+
+        public Seller(int id, string nome, string email, DateTime aniversario, double salarioBase, Department department)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Aniversario = aniversario;
+            SalarioBase = salarioBase;
+            Department = department;
+        }
+
+        public Seller(string nome, string email, DateTime aniversario, double salarioBase, Department department)
+        {
+            Nome = nome;
+            Email = email;
+            Aniversario = aniversario;
+            SalarioBase = salarioBase;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Data >= initial && sr.Data <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,12 +1,17 @@
-using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Linq;
+using SalesWebMvc.Data;
+using System.Globalization;
+//using SalesWebMvc.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc
 {
@@ -23,11 +28,23 @@ namespace SalesWebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationsOption = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
