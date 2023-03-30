4b4437b76f6ea8d719bc6d5ac6063cfd1c62184f
ArleiRodrigues1 <arlei.freire011@gmail.com>
2023-03-30 11:55:24 -0300

    Mudando tema e aplicando estilos

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Insira o nome")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Máximo de 60 e mínimo de 3 caracteres.")]
+        public string Nome { get; set; }
+
+        public Department() { }
+
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+
+    }
+}
